1c4bded53e8a815c305cd0e1137de0482a784b5f
manyajain27 <jainmanya2701@gmail.com>
2024-10-07 22:59:38 +0530

    commit

--- resume_backend/settings.py
+++ resume_backend/settings.py
@@ -199,7 +199,7 @@ DJOSER = {
     'ACTIVATION_URL': 'activate/{uid}/{token}',
     'SEND_ACTIVATION_EMAIL': True,
     'SOCIAL_AUTH_TOKEN_STRATEGY': 'djoser.social.token.jwt.TokenStrategy',
-    'SOCIAL_AUTH_ALLOWED_REDIRECT_URIS': ['http://127.0.0.1:8000/google',],
+    'SOCIAL_AUTH_ALLOWED_REDIRECT_URIS': ['https://manyajain05.pythonanywhere.com/google',],
     'SERIALIZERS': {
         'user_create': 'accounts.serializers.UserCreateSerializer',
         'user': 'accounts.serializers.UserCreateSerializer',
